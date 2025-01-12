f70f88910983c08365af9cae75f8a81d0d4dc8b7
Kayla Hebertson <kayla.hebertson@gmail.com>
2025-01-12 14:38:59 -0700

    debugging tests

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -5,7 +5,7 @@ describe("Quiz", () => {
     it("should start the quiz over once completed", () => {
         cy.get('button').contains('Start Quiz').click();
         for (let i = 0; i < 10; i++) {
-            cy.get('button').contains(i + 1).click();}
+            cy.get('button').contains('1').click();}
         cy.get('button').contains('Take New Quiz').click();
         cy.get('.card').should('be.visible');
         cy.get('h2').should('not.be.empty');
